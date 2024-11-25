af8e2df69249b7e1a9477c07e0d0e0a17ea9aa00
0sugo <josugo26@gmail.com>
2024-11-25 16:10:18 +0300

    Update dashboard

--- src/pages/FrontOffice/FoDashboard.js
+++ src/pages/FrontOffice/FoDashboard.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import { MdAdd } from "react-icons/md";
+import { Link, Navigate, useNavigate } from 'react-router-dom'
+import { MdAdd, MdDelete } from "react-icons/md";
 import newPatient from '../../assets/images/newPatient.svg'
 import eye from '../../assets/images/eye.svg'
+import pen from '../../assets/images/pen.svg'
 
 const FoDashboard = () => {
-
+  const navigate = useNavigate();
   const waitingReviewsData = [
     {
       No: 1,
@@ -45,6 +46,16 @@ const FoDashboard = () => {
     }
   ];
 
+  const dashboardList = [
+    'Add new Patient', 'New Visit', 'Emergency Visit', 'Self Request', 'Appointments'
+  ];
+
+  const handleClick = (data) => {
+    // Format the name: lowercase and remove spaces to get the desired URL format
+    const formattedName = data.replace(/\s+/g, '').toLowerCase(); // Remove spaces and convert to lowercase
+    navigate(`/app/${formattedName}`);
+  };
+
   const [list, setList] = useState(waitingReviewsData);
 
   return (
@@ -55,7 +66,7 @@ const FoDashboard = () => {
         <div className="flex flex-wrap items-center gap-2 justify-between my-4">
           <div>
             <label htmlFor="searchpatient">
-              <input 
+              <input
                 type="search"
                 className='px-3 py-2 bg-white border border-slate-300 placeholder-slate-400 focus:outline-none focus:border-[#0E6F1E] focus:ring-[#0E6F1E] w-full rounded-md focus:ring-1'
                 placeholder='Search for patient here...'
@@ -70,7 +81,7 @@ const FoDashboard = () => {
           </div>
         </div>
 
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
           <div className='bg-[#F3E8FF] p-4 rounded-lg flex items-center gap-4'>
             <div className='bg-[#8D49D4] p-3 rounded-full'>
               <img src={newPatient} alt="patient" />
@@ -80,6 +91,7 @@ const FoDashboard = () => {
               <p>New Visit</p>
             </div>
           </div>
+
           <div className='bg-[#DEF3FF] p-4 rounded-lg flex items-center gap-4'>
             <div className='bg-[#2E8BBF] p-3 rounded-full'>
               <img src={newPatient} alt="patient" />
@@ -89,6 +101,7 @@ const FoDashboard = () => {
               <p>Visits in-progress</p>
             </div>
           </div>
+
           <div className='bg-[#FFE2E5] p-4 rounded-lg flex items-center gap-4'>
             <div className='bg-[#DD3459] p-3 rounded-full'>
               <img src={newPatient} alt="patient" />
@@ -98,6 +111,31 @@ const FoDashboard = () => {
               <p>Completed Visits</p>
             </div>
           </div>
+
+        </div>
+
+        <div>
+          <div className='items-center mt-4 justify-center rounded-lg grid grid-cols-5 gap-3'>
+            {dashboardList.map((data, index) => (
+              <div key={index}>
+                {data === 'Emergency Visit' ? (
+                  <span className='bg-red-700 hover:bg-red-600 w-full cursor-pointer h-full flex py-3 rounded-lg text-white items-center justify-center' onClick={() => handleClick(data)}>
+                    {data}
+                  </span>
+                ) : (
+                  <span
+                    className='bg-[#0E6F1E] hover:bg-green-700 cursor-pointer text-white w-full h-full flex py-3 rounded-lg items-center justify-center'
+                    onClick={() => handleClick(data)}
+                  >
+                    {data}
+                    {console.log(data)}
+                  </span>
+                )}
+              </div>
+            ))}
+
+          </div>
+
         </div>
       </section>
 
@@ -129,9 +167,13 @@ const FoDashboard = () => {
                   <td className="px-6 py-3 text-sm">{data.createdBy}</td>
                   <td className="px-6 py-3 text-sm">{data.time}</td>
                   <td className='py-2 px-6'>
-                    <Link to={`/app/view`}>
+                    {/* <Link to={`/app/view`} >
                       <span className='bg-[#DBFFDE] flex justify-center items-center rounded-lg w-8 h-8 cursor-pointer'><img src={eye} alt='eye' /></span>
-                    </Link>
+                    </Link> */}
+                    <div className='flex justify-center gap-2'>
+                      <span className='bg-[#E4EDFE] flex justify-center items-center rounded-lg w-8 h-8 cursor-pointer' ><img src={pen} alt='pen' /></span>
+                      <span className='bg-[#E4EDFE] flex justify-center items-center rounded-lg w-8 h-8 cursor-pointer' ><MdDelete className='text-red-600 size-5 hover:text-red-500' /> </span>
+                    </div>
                   </td>
                 </tr>
               ))}
@@ -154,6 +196,7 @@ const FoDashboard = () => {
                 <th className="px-6 py-3 text-left text-xs font-semibold">Scheme</th>
                 <th className="px-6 py-3 text-left text-xs font-semibold">Type of Appointment</th>
                 <th className="px-6 py-3 text-left text-xs font-semibold">Consult</th>
+                <th className="px-6 py-3 text-left text-xs font-semibold">Action</th>
               </tr>
             </thead>
             <tbody>
@@ -166,12 +209,18 @@ const FoDashboard = () => {
                   <td className="px-6 py-3 text-sm">{data.scheme}</td>
                   <td className="px-6 py-3 text-sm">{data.createdBy}</td>
                   <td className="px-6 py-3 text-sm">{data.time}</td>
+                  <td className="px-6 py-3 text-sm">
+                    <div className='flex justify-center gap-2'>
+                      <span className='bg-[#E4EDFE] flex justify-center items-center rounded-lg w-8 h-8 cursor-pointer' ><img src={pen} alt='pen' /></span>
+                      <span className='bg-[#E4EDFE] flex justify-center items-center rounded-lg w-8 h-8 cursor-pointer' ><MdDelete className='text-red-600 size-5 hover:text-red-500' /> </span>
+                    </div>
+                  </td>
                 </tr>
               ))}
             </tbody>
           </table>
         </div>
-      </section>
+      </section >
 
       <section className="bg-white p-4 rounded-lg mb-4">
         <span>Appointments</span>
@@ -187,6 +236,7 @@ const FoDashboard = () => {
                 <th className="px-6 py-3 text-left text-xs font-semibold">Scheme</th>
                 <th className="px-6 py-3 text-left text-xs font-semibold">Type of Appointment</th>
                 <th className="px-6 py-3 text-left text-xs font-semibold">Consult</th>
+                <th className="px-6 py-3 text-left text-xs font-semibold">Action</th>
               </tr>
             </thead>
             <tbody>
@@ -199,13 +249,19 @@ const FoDashboard = () => {
                   <td className="px-6 py-3 text-sm">{data.scheme}</td>
                   <td className="px-6 py-3 text-sm">{data.createdBy}</td>
                   <td className="px-6 py-3 text-sm">{data.time}</td>
+                  <td className="px-6 py-3 text-sm">
+                    <div className='flex justify-center gap-2'>
+                      <span className='bg-[#E4EDFE] flex justify-center items-center rounded-lg w-8 h-8 cursor-pointer' ><img src={pen} alt='pen' /></span>
+                      <span className='bg-[#E4EDFE] flex justify-center items-center rounded-lg w-8 h-8 cursor-pointer' ><MdDelete className='text-red-600 size-5 hover:text-red-500' /> </span>
+                    </div>
+                  </td>
                 </tr>
               ))}
             </tbody>
           </table>
         </div>
       </section>
-    </div>
+    </div >
   )
 }
 
