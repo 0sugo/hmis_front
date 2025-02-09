2b3d701ec102e46ed59aac4ca004d922db6def47
0sugo <josugo26@gmail.com>
2025-02-09 16:11:41 +0300

    Update store

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,15 +5,20 @@ import patientReducer from './patient/patientSlice';
 import departmentReducer from './department/departmentSlice';
 import clinicReducer from './clinic/clinicSlice';
 import visitTypesReducer from './visit/visitTypesSlice';
+import bookingReducer from './visit/bookingSlice';
+import serviceReducer from './service/serviceSlice';
 
 const store = configureStore({
   reducer: {
+    // bookings:bookingReducer,
     visits:visitReducer,
     visitTypes:visitTypesReducer,
     schemes:schemeReducer,
     patient:patientReducer,
     departments:departmentReducer,
     clinics:clinicReducer,
+    services:serviceReducer,
+
   },
 });
 
